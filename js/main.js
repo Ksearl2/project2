f91bc768654fa81a9493aa553c5e71f18f7a9f7e
Ksearl2 <ksearle1284@gmail.com>
2025-03-01 12:40:39 -0600

    fixes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 // JS scripts placed here
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.getElementById('offcanvas-menu');
 
-const hamburger = document.querySelector('.hamburger');
-const menu = document.getElementById('main-menu');
-
-hamburger.addEventListener('click', () => {
-  const expanded = hamburger.getAttribute('aria-expanded') === 'true' || false;
-  hamburger.setAttribute('aria-expanded',!expanded);
+menuToggle.addEventListener('click', () => {
+  const expanded = menuToggle.getAttribute('aria-expanded') === 'true' || false;
+  menuToggle.setAttribute('aria-expanded',!expanded);
   menu.classList.toggle('expanded');
 });
